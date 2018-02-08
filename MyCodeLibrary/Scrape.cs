a34b00ce8a27f5936fa4be7e31ca0909f176605e
栗鑫 <lx389029980@outlook.com>
2018-02-08 22:56:09 +0800

    V1

--- MyCodeLibrary/Scrape.cs
+++ MyCodeLibrary/Scrape.cs
@@ -13,14 +13,19 @@ namespace MyCodeLibrary
         public string ScrapeWebpage(string url)
         {
             WebClient client = new WebClient(); // DownloadString()是一个下载网页的方法，位于System.Net命名空间的WebClient类中
-            string reply = client.DownloadString(url);
-            Console.WriteLine(reply);
-
-            File.WriteAllText(@"C:\Users\lx389\Documents\reply.txt", reply);
+            return GetWebpage(url);  
         }
         public string ScrapeWebpage(string url,string filepath)
         {
+            string reply = GetWebpage(url);
+            File.WriteAllText(@"C:\Users\lx389\Documents\reply.txt", reply);
+            return reply;
+        }
 
+        private string GetWebpage( string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
         }
     }
 }
